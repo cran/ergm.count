cb42e3abfb2cbc2661f2552029c6198a09b63bd0
Pavel N. Krivitsky <pavel@uow.edu.au>
2016-03-29 14:51:47 +0000

    version 3.2.2

--- R/zzz.R
+++ R/zzz.R
@@ -1,10 +1,21 @@
+#  File R/zzz.R in package ergm.count, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2008-2016 Statnet Commons
+#######################################################################
 .onAttach <- function(lib, pkg){
   sm <- statnetStartupMessage("ergm.count",c("statnet"),FALSE)
   if(!is.null(sm)){
     packageStartupMessage(sm)
     packageStartupMessage(paste(c(strwrap(paste("NOTE: The form of the term ",sQuote("CMP")," has been changed in version 3.2 of ",sQuote("ergm.count"),". See the news or help('CMP') for more information.",sep="")),""),collapse="\n"))
   }
-
+}
+  
+.onLoad <- function(lib, pkg){    
   .RegisterMHPs()
   .RegisterConstraintImplications()
   .RegisterInitMethods()
--- tests/valued_fit.R
+++ tests/valued_fit.R
@@ -1,3 +1,12 @@
+#  File tests/valued_fit.R in package ergm.count, part of the Statnet suite
+#  of packages for network analysis, http://statnet.org .
+#
+#  This software is distributed under the GPL-3 license.  It is free,
+#  open source, and has the attribution requirements (GPL Section 7) at
+#  http://statnet.org/attribution
+#
+#  Copyright 2008-2016 Statnet Commons
+#######################################################################
 library(ergm.count)
 set.seed(0)
 
@@ -19,4 +28,4 @@ summary(efit)
 true.llk <- sum(dpois(na.omit(c(m)), exp(coef(efit)), log=TRUE)) - sum(dpois(na.omit(c(m)), 1, log=TRUE))
 
 stopifnot(abs(coef(efit)-truth)<0.01)
-stopifnot(abs(true.llk - logLik(efit))<0.1)
+stopifnot(abs(true.llk - logLik(efit))<0.2)
